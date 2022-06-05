3cd0b2e518e362138433af3a24f291f2ed79993f
Dmitry Smirnov <itdismir@gmail.com>
2022-06-05 17:10:10 +0300

    First Project

---
+++ dropdown.js
@@ -0,0 +1,12 @@
+function dropdown() {
+    var dropdown = document.getElementsByClassName("submenu")[0];
+    dropdown.classList.toggle("active");
+
+    var sidebar = document.getElementsByClassName("sidebar")[0];
+    sidebar.classList.toggle("dropdown");
+}
+// there is written dropdown function(is used a toggle method)
+
+$(function() {
+    var tagP = $('div').text('ahahahah');
+});
